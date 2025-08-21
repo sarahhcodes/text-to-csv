5dad5aa2ee128e37d1be285d9f13c8e7e503e577
sarahhcodes <78184189+sarahhcodes@users.noreply.github.com>
2025-08-21 15:10:36 -0600

    adding to do list; creating window

--- project.py
+++ project.py
@@ -2,21 +2,50 @@
 # Currently configured to work with a list of Japanese words formatted as follows:
 # Japanese Word: English Word
 
+# TO DO:
+# add user input for:
+###### number of columns
+###### labels for columns
+###### text file
+###### character used as seperator (':', etc)
+###### save as: "USERINPUT.csv"
+# make column names in make_list() generic
+# make UI look & feel nice
+# function for checking formatting of text file --> runs before other functions & offers suggestions
+# error handling
+# pytest
+
 import csv
+import tkinter
 
 def main():
+    root = tkinter.Tk()
+    root.title("Text to CSV")
+    header = tkinter.Label(root, text="a utility for converting text files to csv")
+    header.pack()
+    root.mainloop()
+
+    #list = make_list("JLPTN3vocab.txt")
+    #make_file(list, "vocab.csv")
+
+
+def make_list(txt):
     words = []
 
-    with open("JLPTN3vocab-short.txt") as file:
+    with open(txt) as file:
         for line in file:
             if ":" in line:
                 japanese, english = line.strip().split(":")
                 words.append({'japanese': japanese.strip(), 'english': english.strip()})
+    
+    return words # returns list of words
 
-    with open("vocab.csv", "a") as file:
-        for word in words:
+
+def make_file(list, export_name):
+    with open(export_name, "a") as file:
+        for item in list:
             writer = csv.writer(file)
-            writer.writerow(word.values())
+            writer.writerow(item.values())
 
 
 if __name__ == "__main__":
