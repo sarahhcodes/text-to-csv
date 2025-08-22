a4d4189dc3c48c9a8be6b0abb69ddbd084ceed28
sarahhcodes <78184189+sarahhcodes@users.noreply.github.com>
2025-08-21 19:30:10 -0600

    adding arguments; making functions generic

--- project.py
+++ project.py
@@ -1,33 +1,35 @@
 # A utility to transform a text file of vocabulary words into a csv file.
-# Currently configured to work with a list of Japanese words formatted as follows:
-# Japanese Word: English Word
 
 # TO DO:
 # add all the flags, including:
-###### number of columns
-###### labels for columns
-###### text file
-###### character used as seperator (':', etc)
-###### save as: "USERINPUT.csv"
-# make column names in make_list() generic
+###### number of columns DONE
+###### labels for columns DONE
+###### text file DONE
+###### character used as seperator (':', etc) DONE
+###### save as: "USERINPUT.csv" DONE
+# make column names in make_list() generic DONE
 # function for checking formatting of text file --> makes sure that file EXISTS and that it's formatted as per user's request
 ###### runs before other functions & offers suggestions
 # error handling
 # pytest
+# docstrings for all functions
 
 import argparse
 import csv
 
 def main():
     parser = argparse.ArgumentParser(description="Convert text files to CSV files")
-    parser.add_argument("-i", "--input", help="text file to input", required=True)
+    parser.add_argument("-i", "--input", help="Text file to input", required=True)
+    parser.add_argument("-c", "--columns", nargs="+", help="Columns to include in CSV")
+    parser.add_argument("-s", "--split_on", help="Character to split on (examples: ':', '-', etc)", required=True)
+    parser.add_argument("-o", "--output", help="Name of CSV file to save as", required=True)
     args = parser.parse_args()
 
-    list = make_list(args.input)
-    make_file(list, "vocab.csv")
+    list = make_list(args.input, args.columns, args.split_on)
+    make_file(list, args.output, args.columns)
 
 
-def make_list(txt):
+def make_list(txt, columns, split_on):
     """
     Creates list of lines in the text file
     
@@ -38,23 +40,25 @@ def make_list(txt):
     :rtype: list
     """
     words = []
-
     # add exception for ValueError - no such file found
     with open(txt) as file:
         for line in file:
             if ":" in line:
-                japanese, english = line.strip().split(":")
-                words.append({'japanese': japanese.strip(), 'english': english.strip()})
-    
+                row = line.strip().split(split_on)
+                dictionary = {}
+                for i in range(len(columns)):
+                    dictionary[columns[i]] = row[i].strip()
+                words.append(dictionary)
+
     return words # returns list of words
 
 
-def make_file(list, export_name):
+def make_file(list, export_name, columns):
     """Converts list to CSV file"""
     with open(export_name, "a") as file:
-        for item in list:
-            writer = csv.writer(file)
-            writer.writerow(item.values())
+        writer = csv.DictWriter(file, fieldnames=columns)
+        writer.writeheader()
+        writer.writerows(list)
 
 
 if __name__ == "__main__":
